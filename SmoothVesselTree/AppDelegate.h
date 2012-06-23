69f2c849beb8bac40e205ebe670f90dadc9b7b4b
Markus Muehlberger <mmuehlberger@me.com>
2012-06-23 11:07:30 +0200

    Initial Commit

---
+++ SmoothVesselTree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmoothVesselTree
+//
+//  Created by Markus Mühlberger on 23.06.12.
+//  Copyright (c) 2012 OneBox Media Solutions GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmoothVesselTree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmoothVesselTree
+//
+//  Created by Markus Mühlberger on 23.06.12.
+//  Copyright (c) 2012 OneBox Media Solutions GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
